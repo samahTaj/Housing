9be382ceef47bded9d6621a50d8f17cb685e0f46
samahTaj <samchicgirl@gmail.com>
2015-03-08 19:15:49 +0200

    edit reg

--- HS/reg.php
+++ HS/reg.php
@@ -1,15 +1,12 @@
 ﻿<?php
     include('db.php');
 	
-	//$selected = mysql_select_db("login", $dbhandle);
+	
 		if(isset($_POST['user']) && isset($_POST['pass'])){
 			$user1 = $_POST['user'];
 			$pass1 = $_POST['pass'];
 			$query = $mysqli->query("SELECT id FROM user WHERE id=$user1");
-			//$query1 = $mysqli->query("SELECT pass FROM user WHERE pass=$pass1");
-			//$r=$query->num_rows;
-			//$r1=$query1->num_rows;
-			//$t=$r +$r1;
+			
 			if( $query->num_rows > 0 ) { //check if there is already an entry for that username
 				echo "Username already exists!";
 				
@@ -17,9 +14,8 @@
 				
 				$mysqli->query("INSERT INTO user (id,pass) VALUES ($user1, $pass1)");
 				echo "<script>				          alert('Welcome.')</script>";
-				//$mysqli->query1("INSERT INTO user (id,pass) VALUES ($user1, $pass1)");
-			     
-				//header("location:reg.php");
+				
+				header("location:login.html");
 				
 			}
 	
@@ -28,36 +24,65 @@
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
   <head>
-    <meta charset="utf-8">
-    <title>Comning soon page: bootstrap free templates</title>
+    <title>Housing Management System</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="bootstrappage.com">
-<!-- Bootstrap style  --> 
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet"/>
-	<link href="assets/css/comingsoon.css" rel="stylesheet"/>
-<!-- Bootstrap style responsive -->
-	<link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet"/>
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- styles -->
+    <link href="css/styles.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
   </head>
-<body>
-<div class="wrapper">
-	<div class="comingsoonBlk">
-	<h1>Register</h1>
-	<p>&nbsp;</p>
-	<div class="socialMedia">
-		&nbsp;&nbsp;
+  <body class="login-bg">
+  	<div class="header">
+	     <div class="container">
+	        <div class="row">
+	           <div class="col-md-12">
+	              <!-- Logo -->
+	              <div class="logo">
+	                 <h1><a href="users.php">Housing Management System    Signup</a></h1>
+	              </div>
+	           </div>
+	        </div>
+	     </div>
 	</div>
-    <form action="ind.php" method="POST">
+
+	<div class="page-content container">
+		<div class="row">
+			<div class="col-md-4 col-md-offset-4">
+				<div class="login-wrapper">
+			        <div class="box">
+			            <div class="content-wrap">
+			                 <form action="login.html" method="POST">
 				<p>Username:</p><input type="text" name="user" />
 				<p>Password:</p><input type="password" name="pass" />
 				<br />
 				<input type="submit" value="Signup!" />
 			</form>
-			<br class="clr">
+			            </div>
+			        </div>
+
+			        
+			    </div>
+			</div>
+		</div>
 	</div>
-</div>
-<div class="copyright">	bootstrappage.com &copy; 2013.</div>
-</body>
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/custom.js"></script>
+  </body>
 </html>
+
+
