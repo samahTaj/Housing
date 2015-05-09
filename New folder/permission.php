11bc6111d2e0d7fde0b0ab16414fa0b8ac26042e
samahTaj <samchicgirl@gmail.com>
2015-05-09 03:52:26 +0300

    delete

--- New folder/permission.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('includes/db.inc');
-include('includes/functions.inc');
-if (login_check($mysqli) != true) header('login.php');
-
-include('body/header.inc');
-include('body/body_permission.inc');
-include ('body/footer.inc');
-?>
